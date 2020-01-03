5486fdbae8ce0c5d58139e6b39265d3fc775648c
Jesse Duffield <jessedduffield@gmail.com>
2020-01-03 16:46:03 +1100

    better usage message

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ func main() {
 }
 
 func usage() {
-	log.Fatal("usage: `horcrux bind [<directory>]` | `horcrux [-t] [-n] split <filename>`")
+	log.Fatal("usage: `horcrux bind [<directory>]` | `horcrux [-t] [-n] split <filename>`\n-n: number of horcruxes to make\n-t: number of horcruxes required to resurrect the original file\nexample: horcrux -t 3 -n 5 split diary.txt")
 }
 
 type horcruxHeader struct {
